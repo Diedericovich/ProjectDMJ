1846964bcfb4530c12475d20c8d1a59fb2fbfa94
JensVanGelder <jens.v.gelder@gmail.com>
2021-01-04 14:41:29 +0100

    Add project files.

---
+++ ProjectDMJ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectDMJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
