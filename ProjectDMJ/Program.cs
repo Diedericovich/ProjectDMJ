786db063332dd50740f2f349902e99a3617edf59
Diederik Van Gogh <doctor_music@hotmail.com>
2021-01-07 16:00:57 +0100

    Create Class Loginmenu

--- ProjectDMJ/Program.cs
+++ ProjectDMJ/Program.cs
@@ -14,9 +14,8 @@ namespace ProjectDMJ
             Menus menus = new Menus();
             menus.ShowMenu(gameLibrary);
 
-            //Users user = new Users();
-            //List<Users> users = new List<Users>();
-            //List<Games> userLibrary = new List<Games>();
+
+            List<Games> userLibrary = new List<Games>();
             //dataManager.ReadUsersFile(users, dataManager.pathUsersDataFile);
             //UserMenus userMenus = new UserMenus();
 
